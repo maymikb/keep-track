de35af6f115fc6e52814db06e2261f7928c6fa70
maymikb <123998301+maymikb@users.noreply.github.com>
2023-12-21 15:32:13 -0800

    terminal note

--- Develop/server.js
+++ Develop/server.js
@@ -3,11 +3,18 @@ const PORT = 3001
 const app =express()
 const path =require("path")
 
+app.use(express.static("public"))
 
 app.get("/", function(req,res){
 
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
+
+app.get("/notes", function(req,res){
+
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
+})
+
 
 
 
