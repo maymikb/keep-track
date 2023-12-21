7197f0fcf7ee00017c0412adacfa7369acb54b20
maymikb <123998301+maymikb@users.noreply.github.com>
2023-12-21 15:36:33 -0800

    added api/notes route

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,11 @@ app.get("/notes", function(req,res){
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
+app.get("/api/notes", function(req,res){
+
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+})
+
 
 
 
