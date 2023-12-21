a1ce78057e9f46975f6caec0d1e8e88fffb86d3c
maymikb <123998301+maymikb@users.noreply.github.com>
2023-12-21 15:49:21 -0800

    addpost file

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,14 @@
 const express = require ("express");
 const PORT = 3001
 const app =express()
+const fs = require("fs")
 const path =require("path")
 
+// middlewares
 app.use(express.static("public"))
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 
 app.get("/", function(req,res){
 
@@ -19,6 +24,30 @@ app.get("/api/notes", function(req,res){
 
     res.sendFile(path.join(__dirname, "/db/db.json"))
 })
+
+app.post("/api/notes", function(req,res){
+    console.log(req.body)
+
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text
+    }
+
+    fs.readFile("./db/db.json", "utf-8", function(err, data) {
+        const notes = JSON.parse(data);
+
+        notes.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 4), function() {
+            console.log("Updated db.json!")
+
+            res.send("Added new note!")
+        })
+
+    })
+    
+})
+
 
 
 
